77f65060a928ae8f3a4a51d3d141a1f90030547c
Rohit raj <rohit1201.be21@chitkara.edu.in>
2026-07-18 00:43:23 +0530

    today practice use basemodal and make a starter of apis

--- Backend/Book-Model/main.py
+++ Backend/Book-Model/main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI , HTTPException
+from pydantic import BaseModel , Field
+from datetime import datetime
 
 app = FastAPI()
 
-
 class Book(BaseModel):
-    id: int
+    
     title: str
     author: str
-    price: int
+    price: int=Field(gt=0, description="Price must be greater than zero")
+
+books = [
+    {
+        "id":1,
+        "title":"Clean Code",
+        "author":"Robert Martin",
+        "price":799
+    }
+]
 
 
 @app.get("/")
@@ -16,19 +25,41 @@ def home():
     return {"message": "Welcome to the Book API"}
 
 
-books = [{"id": 1, "title": "Clean Code", "author": "Robert Martin", "price": 799}]
-
 
 @app.get("/books")
 def getBook():
     return books
 
 
+from datetime import datetime
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy",
+        "service": "Book API",
+        "timestamp": datetime.now()
+    }
+    
 @app.get("/books/{id}")
-def getBookById(bookid: int):
+def getBookById(id: int):
     for book in books:
-        if book["id"] == bookid:
+        if book["id"] == id:
             return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
+#search book by author
+
+@app.get("/books/author")
+def getBookByAuthor(author: str):
+    result = []
+    for book in books:
+        if book["author"].lower() == author.lower():
+            result.append(book)
+    if not result:
+        raise HTTPException(status_code=404, detail="No books found by this author")
+    return result
 
 
 @app.post("/books")
@@ -44,25 +75,33 @@ def addBooks(book: Book):
 
 
 @app.put("/books/{id}")
-def update_book(book_id: int, book: Book):
+def update_book(id: int, book: Book):
     for b in books:
-        if b["id"] == book_id:
+        if b["id"] == id:
             b["title"] = book.title
             b["author"] = book.author
             b["price"] = book.price
             return {"message": "Book updated", "book": b}
+    
+    raise HTTPException(status_code=404, detail="Book not found")
         
    
         
 @app.delete("/books/{id}")
-def update_book(book_id: int, book: Book):
+def update_book(id: int, book: Book):
     for b in books:
         
-        if book["id"] == book_id:
+        if book["id"] == id:
             deleted_book = books.pop(i)
             return {
                 "message": "Book deleted successfully",
                 "book": deleted_book
             }
+            
+    raise HTTPException(status_code=404, detail="Book not found")
         
         
+        
+@app.get("/books/count")
+def books_count(book:Book):
+    return len(books)
